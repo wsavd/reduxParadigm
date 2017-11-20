5764ae63a97e58289327e8ef69760ced2f7a0814
wsavd <tuve@rootfest.net>
2017-11-20 12:35:36 +0300

    store.getState() в консоле выведет массив todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {createStore} from 'redux'
+import {todos} from './todos'
+import {Provider} from 'react-redux'
+
+const reducer = (state = todos , action) => {
+  return state
+}
+
+const store = createStore(reducer)
+store.subscribe(() => { console.log(store.getState()) })
+window.store = store
 
 const App = () => {
   return(
-    <div>
-      App
-    </div>
+    <Provider store={store}>
+      <div>
+        App
+      </div>
+    </Provider>
   )
 }
 
